2e3769c94bfbdb979a74074a0f3566ec7489cfc3
jungwoogyun <jwg8910@naver.com>
2024-07-16 14:42:01 +0900

    .

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("HELLO WORLD TEST.....");
+
+//-------------------------------
+//LODASH
+//-------------------------------
+import _ from 'lodash';
+
+const result = _.join(['Hello', 'Parcel'], ' ');
+console.log(result); // 출력: "Hello Parcel"
